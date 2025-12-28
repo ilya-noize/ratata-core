acb3c99414174b513f46ba8b66eed5c49f6bf136
Ilya Shumakov <shumakoff.ilya@yandex.ru>
2025-12-28 23:56:19 +0300

    add DeadlockExample

---
+++ src/main/java/school/sorokin/javacore/multithreading/deadlock/DeadlockExample.java
@@ -0,0 +1,36 @@
+package school.sorokin.javacore.multithreading.deadlock;
+
+public class DeadlockExample {
+    private static final int TRIES = 100;
+
+    public static void main(String[] args) {
+        Object firstSync = new Object();
+        Object secondSync = new Object();
+
+        Thread firstThread = new Thread(() -> {
+            for (int i = 0; i < TRIES; i++) {
+                synchronized (firstSync) {
+                    System.out.println("Первый поток захватил первый монитор");
+                    synchronized (secondSync) {
+                        System.out.println("Первый поток захватил второй монитор");
+                        System.out.println("---");
+                    }
+                }
+            }
+        });
+        Thread secondThread = new Thread(() -> {
+            for (int i = 0; i < TRIES; i++) {
+                synchronized (secondSync) {
+                    System.out.println("Второй поток захватил второй монитор");
+                    synchronized (firstSync) {
+                        System.out.println("Второй поток захватил первый монитор");
+                        System.out.println("---");
+                    }
+                }
+            }
+        });
+
+        firstThread.start();
+        secondThread.start();
+    }
+}
